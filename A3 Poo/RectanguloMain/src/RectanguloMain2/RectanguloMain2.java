2be9c48c0816addf0dd3878bfe8c31fa74c4e12b
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2024-11-29 13:21:01 +0100

    Articulo y punto2

--- A3 Poo/RectanguloMain/src/RectanguloMain2/RectanguloMain2.java
+++ A3 Poo/RectanguloMain/src/RectanguloMain2/RectanguloMain2.java
@@ -4,7 +4,9 @@
  */
 package RectanguloMain2;
 
+import Objetos.Articulo;
 import Objetos.Rectangulo;
+import java.util.Scanner;
 
 /**
  *
@@ -20,6 +22,23 @@ public class RectanguloMain2 {
         
         System.out.println("Area "+ rect.getArea());
         System.out.println(rect);
+        Articulo tienda = new Articulo("Bicis", 10, 15);
+        System.out.println(tienda);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantas quieres?");
+        int compra = sc.nextInt();
+        tienda.vender(compra);
+        System.out.println("Cuantas quieres almacenar?");
+        int mas_stock = sc.nextInt();
+        if (tienda.almacenar(mas_stock))
+        {
+            System.out.println("Almacenadas");
+        }
+        else
+        {
+            System.out.println("No se pueden almacenar");
+        }
+        System.out.println("Te quedan " + tienda.getCuantosQuedan());
     }
     
 }
