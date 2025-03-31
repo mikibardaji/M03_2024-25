b646a8af0312a146f59f44c2cc0ba78892750a0d
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2025-03-31 14:01:58 +0200

    script amb comentaris

--- A5 BD/TikTok/ScriptBD/ScriptBD.sql
+++ A5 BD/TikTok/ScriptBD/ScriptBD.sql
@@ -8,3 +8,17 @@ CREATE TABLE videos (
     likes INT UNSIGNED DEFAULT 0 CHECK (likes >= 0),       -- Nombre de "M'agrada", sempre positiu o 0
     durada DOUBLE NOT NULL CHECK (durada > 0)  -- Duració del vídeo en segons (ha de ser positiva)
 );
+DROP TABLE observadores;
+CREATE TABLE observadores (
+    nick VARCHAR(255) NOT NULL PRIMARY KEY,       -- nick usuari 
+    nombre VARCHAR(255) NOT NULL   
+);
+DROP TABLE COMENTARIOS;
+CREATE TABLE COMENTARIOS (
+	video_id INT NOT NULL,
+    observador_nick VARCHAR(255) NOT NULL ,       -- nick usuari 
+    comentario VARCHAR(255) NOT NULL ,
+    PRIMARY KEY (observador_nick, video_id),  -- Clau primària
+    FOREIGN KEY (observador_nick) REFERENCES observadores(nick) ON DELETE CASCADE,
+    FOREIGN KEY (video_id) REFERENCES videos(id) ON DELETE CASCADE
+);
